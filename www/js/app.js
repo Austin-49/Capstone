f27beef67d2b574949e521a09d5ac794a677da1a
Austin-49 <austin-94@hotmail.com>
2015-06-20 14:11:12 -0700

    Finished App!
    Fixed numerous bugs, cleaned up formatting, added help Pages

--- www/js/app.js
+++ www/js/app.js
@@ -23,7 +23,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ui.utils', 'ngCordov
 	
 	var getData = function(callback) {
 		/* Store the current date to a variable so we know when we last updated the program info
-		   We check to see if the user has either no data or no updat timestamp. If they're missing either
+		   We check to see if the user has either no data or no update timestamp. If they're missing either
 		we give them a date back in 1980 so that the app will automatically give them new data on startup */ 
 		if (typeof window.localStorage['lastUpdate'] === 'undefined' || typeof window.localStorage['sharedArray'] === 'undefined') {
 			window.localStorage['lastUpdate'] = JSON.stringify("198001010000");
@@ -42,7 +42,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ui.utils', 'ngCordov
 				callback();
 			} else {
 				console.log("Data is up to date");
-				console.log(data);
 				// Return back to controller.js enabling the app to begin using the aquired data
 				callback();
 			}
@@ -72,26 +71,49 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ui.utils', 'ngCordov
 	this.advSearch = function(array, callback) {
 		programs = JSON.parse(window.localStorage['sharedArray']);
 		queryResults = [];
+		var catMatch = false;
+		//DaysWeekFound is a boolean which further down in the function is set to true if a program was found that
+		//contains a day of the week that the user specified.
 		daysWeekFound = false;
+		//Cycle through the array sent through by controller.js, each iteration through this loop is a new query
 		for(i=0;i<array.length;i++) {
+			//Iterate through the programs data, each iteration through this loop is a new category 
 			for(k=0;k<programs.length;k++) {
-				if(array[i].category == "" || programs[k].category.toLowerCase() == array[i].category.toLowerCase()) {
+				if(array[i].category == "" || programs[k].category.toLowerCase().indexOf(array[i].category.toLowerCase())>=0) {
+					catMatch = true;
+				} else {
+					catMatch = false;
+				} 
+				//If the category parameter set in the advanced search page isn't set we're forced to cycle through
+				//every category, otherwise we delve into this category array and after cycling through all data we
+				//break out, moving onto another query
+					//We need to get the size of an object in javascript so we iterate through the values within
+					//the category array with variable Y becoming the size of the nested array.
 					var y = 0;
 					for(var x in programs[k]) {
 						y++;
 					}
-					//2 offeset is due to position ID and name of the array
+					//Now that we have the size we iterate through each ageGroup array, we reduce total size by two
+					//due to the fact that each array holds 2 values which aren't arrays, ID and name.
 					for(j=0;j<(y-2);j++) {
+					//Again get the size of the nested array by iterating, M becomes the size of the array.
 						var m = 0;
 						for(var x in programs[k][j]) {
 							m++;
 						}
+						//Delve into each programName array
 						for(l=0;l<(m-2);l++) {
+							if(array[i].category == "" || catMatch == true || programs[k][j][l].name.toLowerCase().indexOf(array[i].category.toLowerCase())>=0) {
+							//Get the size of nested arrays
 							var n = 0;
 							for(var x in programs[k][j][l]) {
 								n++;
 							}
+							//Finally go into the array which contains the information regarding a program
 							for(t=0;t<(n-2);t++) {
+								//Iterate through the daysWeek array, which holds all the days of the week
+								//the user wanted their activity to fall on. If the regex matches one of those
+								//values to the days in which a program is offered we set the boolean to true
 								for(var p =0; p<array[i].daysWeek.length; p++) {
 									var regex = new RegExp( array[i].daysWeek[p], 'i');
 									if(programs[k][j][l][t].progDays.match(regex)) {
@@ -99,13 +121,22 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ui.utils', 'ngCordov
 										break;
 									}
 								}
-								if((array[i].location == "" || programs[k][j][l][t].location.toLowerCase().indexOf(array[i].location.toLowerCase()) > 0) &&
-								   (array[i].instructor == "" || programs[k][j][l][t].instructor.toLowerCase().indexOf(array[i].instructor.toLowerCase()) > 0) &&
-								   (array[i].municipality == "" || programs[k][j][l][t].municipality.toLowerCase().indexOf(array[i].municipality.toLowerCase()) >0) &&
+								//Check all other values to see if what the user inputed matches anything to due with the values in
+								//the program info. If any field is left blank it will match all cases.
+								if((array[i].location == "" || programs[k][j][l][t].location.toLowerCase().indexOf(array[i].location.toLowerCase()) >= 0) &&
+								   (array[i].instructor == "" || programs[k][j][l][t].instructor.toLowerCase().indexOf(array[i].instructor.toLowerCase()) >= 0) &&
+								   (array[i].municipality == "none" || programs[k][j][l][t].municipality.toLowerCase() == array[i].municipality.toLowerCase()) &&
+								   //Age is matched by checking to see if the age the user inputed is between a max and min value which
+								   //are calculated and parsed on the server side as two seperate values. If the input value is NaN,
+								   //the user didn't input anything so we match
 								   (isNaN(array[i].age) || (array[i].age <= programs[k][j][l][t].ageMax && array[i].age >= programs[k][j][l][t].ageMin)) &&
+								   //If the program is offered daily it will match all cases so we continue
 								   (programs[k][j][l][t].progDays == "Daily" || daysWeekFound == true) &&
 								   (programs[k][j][l][t].startDate >= array[i].startDayMonth && programs[k][j][l][t].startDate <= array[i].endDayMonth))  
 									{
+									//Push relevant details involving the programs that did match into a JSON object
+									//The query that matched this is added onto the end so that in the results page
+									//a listing of parameters which matched a program can be seen.
 									queryResults.push({name: programs[k][j][l].name,
 													category: programs[k].category,
 													categoryPos: k,
@@ -114,16 +145,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ui.utils', 'ngCordov
 													query: array[i]});
 									
 								}
+								//Reset the variable so the next program isn't immediately matched.
 								daysWeekFound = false;								
 							}
 						}
+						}
 					}
-					if(array[i].category == programs[k].category) {
-						break;
-					}
-				} 
 			}
 		}
+		//return to controller.js
 		callback();
 	}
 }])
@@ -144,18 +174,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ui.utils', 'ngCordov
     abstract: true,
     templateUrl: "templates/menu.html",
     controller: 'AppCtrl'
-  })
-
-  // Create a search page, with no caching so that previous searches aren't saved
-  .state('app.search', {
-    cache: false,
-    url: "/search",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/search.html",
-		controller:'progSearchCtrl'
-      }
-    }
   })
 
   //Create the advanced search page, disabling caching so that the DOM created by clicking the 'Add Query'
@@ -188,7 +206,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ui.utils', 'ngCordov
   
   .state('app.progListCategories', {
       url: "/programs",
-	  cache: false,
       views: {
         'menuContent': {
           templateUrl: "templates/progListCategories.html",
@@ -199,7 +216,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ui.utils', 'ngCordov
 
   .state('app.progListAgeGroup', {
     url: "/programs/:programCategory",
-	cache: false,
     views: {
       'menuContent': {
         templateUrl: "templates/progListAgeGroup.html",
@@ -210,7 +226,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ui.utils', 'ngCordov
   
   .state('app.progListNames', {
     url: "/programs/:programCategory/:programAgeGroup",
-	cache: false,
     views: {
       'menuContent': {
         templateUrl: "templates/progListNames.html",
@@ -221,7 +236,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ui.utils', 'ngCordov
   
     .state('app.progListInfo', {
     url: "/programs/:programCategory/:programAgeGroup/:programName",
-	cache: false,
     views: {
       'menuContent': {
         templateUrl: "templates/progListInfo.html",
@@ -248,6 +262,46 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ui.utils', 'ngCordov
       'menuContent': {
         templateUrl: "templates/progLocation.html",
         controller: 'progListInfoCtrl'
+      }
+    }
+  })
+  
+     .state('app.tutorial', {
+    url: "/tutorial",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/tutorial.html",
+        controller: 'helpCtrl'
+      }
+    }
+  })
+  
+    .state('app.helpSearch', {
+    url: "/tutorial/helpSearch",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/helpSearch.html",
+        controller: 'helpCtrl'
+      }
+    }
+  })
+  
+    .state('app.helpRegister', {
+    url: "/tutorial/helpRegister",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/helpRegister.html",
+        controller: 'helpCtrl'
+      }
+    }
+  })
+  
+    .state('app.helpProgram', {
+    url: "/tutorial/helpProgram",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/helpProgram.html",
+        controller: 'helpCtrl'
       }
     }
   })
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,14 +11,17 @@ angular.module('starter.controllers', [])
 	Data.getData(callback);
 	
 	$scope.DropIn = function() {
+		//Upon clicking the Drop-in button, redirection to Fit-in-Fitness is required
 		var confirmPopup = $ionicPopup.confirm({
 		 title: 'Redirecting to Fit-In-Fitness',
 		 template: 'Drop-in Information is displayed on an external page, Do you wish to continue?'
 		});
+		//If the user clicks yes to being redirected, we take them to fit-in-fitness.ca
 		confirmPopup.then(function(res) {
 		 if(res) {
 		   window.open('http://www.fitinfitness.ca/schedules.html', '_system', 'location=no'); return false;
 		 } else {
+		 //Otherwise break out
 		   return;
 		 }
 	   });
@@ -40,7 +43,17 @@ angular.module('starter.controllers', [])
 	// Checks ahead of time to see if there is only a single age group for the given category
 	// If there is we skip the agegroups page and go straight to program name listing
 	$scope.ageGroups = function(category) {
-		$state.go('app.progListNames', {programCategory: category, programAgeGroup: ageGroup});
+	var n = 0;
+	for(var x in $scope.programs[category]) {
+		n++;
+	}
+	console.log(n);
+	if(n == 4) {
+		$state.go('app.progListNames', {programCategory: category, programAgeGroup: $scope.programs[category][0].ageId});
+	} else {
+	$state.go('app.progListAgeGroup', {programCategory: category});
+	}
+		
 	}
 	
 	// The button which links the info page to the google maps location page
@@ -59,15 +72,18 @@ angular.module('starter.controllers', [])
 	$scope.barcode = $stateParams.programBarcode;
 	$scope.website = $stateParams.programWebsite;
 	
+	//Upon clicking register a pop-up appears asking if you wish to go to Rec Online
 	$scope.RecOnline = function(website) {
 		var confirmPopup = $ionicPopup.confirm({
 		 title: 'Redirecting to RecOnline',
 		 template: 'Registration requires you to go to an external page, do you wish to continue?'
 		});
+		// If you click yes, redirect the user to Rec Online using the appropriate website attached to the program
 		confirmPopup.then(function(res) {
 		 if(res) {
-		   window.open(website, '_system', 'location=no'); return false;
+		   window.open(website, '_system', 'location=yes'); return false;
 		 } else {
+		 //Otherwise break out
 		   return;
 		 }
 	   });
@@ -75,6 +91,7 @@ angular.module('starter.controllers', [])
 })
 
 .controller('progAdvSearchCtrl', function($scope, $compile, $rootScope, $stateParams, $state, MyService, $ionicScrollDelegate, $ionicHistory) {
+	// Callback function after searching, redirects you to results page and resets the query
 	function callback() {
 		$scope.query = [];	
 		$state.go('app.advResults', {}, { reload: true });
@@ -82,6 +99,7 @@ angular.module('starter.controllers', [])
 	$scope.query = [];
 	$scope.queryNum = 1;
 	
+	// When clicking Schedule Options this function swaps the class name to hide and show, to display and hide the div
 	$scope.showList = function(id) {
 		if(document.getElementById("daysWeek" + id).className == "ng-hide") {
 			document.getElementById("daysWeek" + id).className = "ng-show";
@@ -90,96 +108,131 @@ angular.module('starter.controllers', [])
 		}
 	};
 	
+	// Function responsible for submitting information to be queried
 	$scope.submit = function() {
+		// Grab each query on the current page
 		var divs = document.getElementById('main').getElementsByTagName('ion-item');
 		var daysWeek = [];
+		//Cycle through each query grabbing all of its children (Which is the input fields on the page)
 		for (var i=0; i<divs.length; i++) {
+		
+			//divs[i].children[1] is the age search parameter, divs[i].children[2] is the granularity parameter,
+			//If no age was set we void the granularity parameter
 			if (divs[i].children[1].value == "") {
 				divs[i].children[2].value = "";
 			}
 			
+			// If the user inputed Months as their age granularity, divide the age value by 12
 			if (divs[i].children[2].value == "Months") {
 				divs[i].children[1].value = (divs[i].children[1].value/12);
 			}
-			console.log(divs[i].children);
-			for(var j=0; j<divs[i].children[8].children.length; j++) {
-				if(divs[i].children[8].children[j].type == "checkbox") {
-					if(divs[i].children[8].children[j].checked) {
-						daysWeek.push(divs[i].children[8].children[j].value);
+			
+			//div[i].children[8] is the days of the Week check boxes + dates, since there is a <br> tag in between
+			//Each checkbox we cycle through div[i].children[9]'s children to find which are checkboxes.
+			//Once found we push the checked values into an array. This array becomes a listing of the days
+			//of the week the user selected.
+			for(var j=0; j<divs[i].children[9].children.length; j++) {
+				if(divs[i].children[9].children[j].type == "checkbox") {
+					if(divs[i].children[9].children[j].checked) {
+						daysWeek.push(divs[i].children[9].children[j].value);
 					}
 				}
 			}
-			var Index1 = divs[i].children[8].children[17].selectedIndex;
-			var Index2 = divs[i].children[8].children[18].selectedIndex;
-			var Index3 = divs[i].children[8].children[21].selectedIndex;
-			var Index4 = divs[i].children[8].children[22].selectedIndex;
+			console.log(divs[i].children);
+			// divs[i].children[9].children[17]/[18] is the first Month + Day selector
+			// divs[i].children[9].children[21]/[22] is the second Month + Day selector
+			// The following block finds the indexes for these values to be used in the next block to find the strings
+			var Index1 = divs[i].children[9].children[17].selectedIndex;
+			var Index2 = divs[i].children[9].children[18].selectedIndex;
+			var Index3 = divs[i].children[9].children[21].selectedIndex;
+			var Index4 = divs[i].children[9].children[22].selectedIndex;
 			
-			var TextString = divs[i].children[8].children[17].children[Index1].outerText;
-			TextString = TextString + " " + divs[i].children[8].children[18].children[Index2].outerText;
-			TextString = TextString + " - " + divs[i].children[8].children[21].children[Index3].outerText;
-			TextString = TextString + " " + divs[i].children[8].children[22].children[Index4].outerText;
-			console.log(TextString);
+			//To create a nice text string which says what the user searched by we get the text value from the indexes
+			//found in the previous block
+			var TextString = divs[i].children[9].children[17].children[Index1].outerText;
+			TextString = TextString + " " + divs[i].children[9].children[18].children[Index2].outerText;
+			TextString = TextString + " - " + divs[i].children[9].children[21].children[Index3].outerText;
+			TextString = TextString + " " + divs[i].children[9].children[22].children[Index4].outerText;
 			
-			var startDayMonth = divs[i].children[8].children[17].value + divs[i].children[8].children[18].value;
+			//Grab the start date the user inputed and the end date, parsing them into ints so that they can be compared
+			//Dates within the app are represented as a single integer with the month being first and the day following.
+			//For example February 21 would become 221. This allows us to determine whether the date a program is offered
+			//Is between the two dates the user inputed by simply seeing if the number is between the two.
+			var startDayMonth = divs[i].children[9].children[17].value + divs[i].children[9].children[18].value;
 			startDayMonth = parseInt(startDayMonth);
-			var endDayMonth = divs[i].children[8].children[21].value + divs[i].children[8].children[22].value;
+			var endDayMonth = divs[i].children[9].children[21].value + divs[i].children[9].children[22].value;
 			endDayMonth = parseInt(endDayMonth);
+			
+			//$scope.query is the JSON array sent through to the Advanced Search function, the sets of data are compared
+			//against the activity listing to see what matches and what dosen't
 			
 			$scope.query.push({
 							category: divs[i].children[0].value,
 							age: parseInt(divs[i].children[1].value),
 							ageGranularity: divs[i].children[2].value,
-							municipality: divs[i].children[3].value,
-							location: divs[i].children[4].value,
-							instructor: divs[i].children[5].value,
+							municipality: divs[i].children[6].value,
+							location: divs[i].children[5].value,
+							instructor: divs[i].children[4].value,
 							daysWeek: daysWeek,
 							startDayMonth: startDayMonth,
 							endDayMonth: endDayMonth,
 							scheduleText: TextString
 							});	
-			console.log($scope.query);
 			daysWeek = [];
 		}
+		//Send the query data to the advanced search function
+		console.log($scope.query);
 		MyService.advSearch($scope.query, callback);
 		
 	};
 	
+	//This function is responsible for recreating all the HTML DOM into another query so the user may refine/add
+	//to their search
 	$scope.newQuery = function() {
 
 		var button = document.getElementById("query");
 		
+		//Get the list which contains all queries within it so that it may all be added to the list after, creating
+		//a single new query on the list.
+		//Container is a new ion-item which will hold all the DOM elements and then be appended to main at the end.
 		var main = document.getElementById('main').getElementsByClassName('list');
 		var container = document.createElement('ion-item');
 		container.className = ("item");
 		
+		//Recreate the category search
 		var category = document.createElement('input');
 		category.type = "search";
 		category.placeholder="Activity...";
 		category.id="searchCategory";
 		
+		//Recreate the age search
 		var age = document.createElement('input');
 		age.type = "search";
 		age.placeholder="Age...";
 		age.id="searchAge";
 		age.style.display = "inline-block";
 		
+		//Recreate the ageGranularity search
 		var ageGranularity = document.createElement('SELECT');
 		ageGranularity.id="searchAgeGranularity";
 		ageGranularity.style.display = "inline-block";
 		
+		//With years option
 		var year = document.createElement('option');
 		year.text = "Years";
 		ageGranularity.add(year);
 		
+		//With months option
 		var month = document.createElement('option');
 		month.text = "Months";
 		ageGranularity.add(month);
 		
+		//Recreate the hidden div which holds days of the week and date picker
 		var schedule = document.createElement('div');
 		schedule.id = "daysWeek" + $scope.queryNum;
 		schedule.className = "ng-hide";
 		
-		
+		//The daysButton is the button which will display the hidden div from above when clicked
 		var daysButton = document.createElement('button');
 		daysButton.className = "button button-block button-positive";
 		daysButton.style.width = "180px";
@@ -188,8 +241,11 @@ angular.module('starter.controllers', [])
 		$compile(daysButton)($scope);
 		$scope.queryNum++;
 		
+		//Recreate the days of the week checkboxes
 		schedule.appendChild(document.createTextNode(" Available on the following days"));
 		schedule.appendChild(document.createElement('br'));
+		
+		//With Sunday option
 		var day = document.createElement('input');
 		day.type = "checkbox";
 		day.checked = "checked";
@@ -198,6 +254,7 @@ angular.module('starter.controllers', [])
 		schedule.appendChild(document.createTextNode(" Sunday"));
 		schedule.appendChild(document.createElement('br'));
 		
+		//With Monday option
 		var day = document.createElement('input');
 		day.type = "checkbox";
 		day.checked = "checked";
@@ -206,6 +263,7 @@ angular.module('starter.controllers', [])
 		schedule.appendChild(document.createTextNode(" Monday"));
 		schedule.appendChild(document.createElement('br'));
 		
+		//With Tuesday option
 		var day = document.createElement('input');
 		day.type = "checkbox";
 		day.checked = "checked";
@@ -214,6 +272,7 @@ angular.module('starter.controllers', [])
 		schedule.appendChild(document.createTextNode(" Tuesday"));
 		schedule.appendChild(document.createElement('br'));
 		
+		//With Wedensday option
 		var day = document.createElement('input');
 		day.type = "checkbox";
 		day.checked = "checked";
@@ -222,6 +281,7 @@ angular.module('starter.controllers', [])
 		schedule.appendChild(document.createTextNode(" Wednesday"));
 		schedule.appendChild(document.createElement('br'));
 		
+		//With Thursday option
 		var day = document.createElement('input');
 		day.type = "checkbox";
 		day.checked = "checked";
@@ -230,6 +290,7 @@ angular.module('starter.controllers', [])
 		schedule.appendChild(document.createTextNode(" Thursday"));
 		schedule.appendChild(document.createElement('br'));
 		
+		//With Friday option
 		var day = document.createElement('input');
 		day.type = "checkbox";
 		day.checked = "checked";
@@ -238,6 +299,7 @@ angular.module('starter.controllers', [])
 		schedule.appendChild(document.createTextNode(" Friday"));
 		schedule.appendChild(document.createElement('br'));
 		
+		//With Saturday option
 		var day = document.createElement('input');
 		day.type = "checkbox";
 		day.checked = "checked";
@@ -248,12 +310,14 @@ angular.module('starter.controllers', [])
 		schedule.appendChild(document.createElement('br'));
 		schedule.appendChild(document.createElement('br'));
 		
+		//Recreate The first Month picker
 		var startMonth = document.getElementById('startMonth');
 		var clnSMonth = startMonth.cloneNode(true);
 		schedule.appendChild(document.createTextNode(" Available between"));
 		schedule.appendChild(document.createElement('br'));
 		schedule.appendChild(clnSMonth);
 		
+		//Recreate The first Day picker
 		var startDay = document.getElementById('startDay');
 		var clnSDay = startDay.cloneNode(true);
 		schedule.appendChild(clnSDay);
@@ -261,24 +325,34 @@ angular.module('starter.controllers', [])
 		schedule.appendChild(document.createElement('br'));
 		schedule.appendChild(document.createTextNode(" and "));
 		schedule.appendChild(document.createElement('br'));
+		
+		//Recreate The end Month picker
 		var endMonth = document.getElementById('endMonth');
 		var clnEMonth = endMonth.cloneNode(true);
 		schedule.appendChild(clnEMonth);
 		
+		//Recreate The end Day picker
 		var endDay = document.getElementById('endDay');
 		var clnEDay = endDay.cloneNode(true);
 		schedule.appendChild(clnEDay);
 		
+		//Recreate The municipality input
 		var municipality = document.createElement('input');
 		municipality.type = "search";
 		municipality.placeholder="Municipality...";
 		municipality.id="searchCategory";
 		
+		
+		var municipality = document.getElementById('municipality');
+		var clnMunic = municipality.cloneNode(true);
+		
+		//Recreate The location input
 		var location = document.createElement('input');
 		location.type = "search";
 		location.placeholder="Facility...";
 		location.id="searchLocation";
 		
+		//Recreate The first instructor input
 		var instructor = document.createElement('input');
 		instructor.type = "search";
 		instructor.placeholder="Instructor...";
@@ -286,18 +360,21 @@ angular.module('starter.controllers', [])
 		instructor.style.display = "inline-block";
 
 
+		//Append all the newly created DOM objects to the ion-item
 		container.appendChild(category);
 		container.appendChild(age);
 		container.appendChild(ageGranularity);
-		container.appendChild(municipality);
-		container.appendChild(location);
+		container.appendChild(document.createElement('br'));
 		container.appendChild(instructor);
+		container.appendChild(location);
+		container.appendChild(clnMunic);
 		container.appendChild(document.createElement('br'));
 		container.appendChild(daysButton);
 		container.appendChild(schedule);
 		container.appendChild(button);
 		
 	
+		//Append the ion-item to the list, creating a new query
 		main[0].appendChild(container);
 		
 		$ionicScrollDelegate.scrollBottom();
@@ -306,15 +383,12 @@ angular.module('starter.controllers', [])
 
 .controller('progAdvResultsCtrl', function($scope, MyService) {
 	$scope.results = MyService.getQuery();
-	console.log($scope.results);
 })
 
-.controller('progSearchCtrl', function($scope, MyService) {
-	// Nothing required, remove?
-})
-
-.controller('helpCtrl', function($scope, MyService) {
-	// Nothing required, remove?
+.controller('helpCtrl', function($scope, MyService, $state) {
+	$scope.regTutorial = function() {
+		$state.go('app.helpRegister');
+	};
 })
 
 .controller('locationCtrl', function($scope, $rootScope, $stateParams, MyService) {
